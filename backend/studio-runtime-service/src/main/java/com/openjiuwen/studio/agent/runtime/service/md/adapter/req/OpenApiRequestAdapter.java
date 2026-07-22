bdfcde5013ac005539e66ec4ef058b226fd25a67
dehaiwang <wangdehai@huawei.com>
2026-07-22 15:30:32 +0800

    studio2.0中增加千问rerank模型失败，且模型调试不能成功

--- backend/studio-runtime-service/src/main/java/com/openjiuwen/studio/agent/runtime/service/md/adapter/req/OpenApiRequestAdapter.java
+++ backend/studio-runtime-service/src/main/java/com/openjiuwen/studio/agent/runtime/service/md/adapter/req/OpenApiRequestAdapter.java
@@ -4,14 +4,8 @@
 
 package com.openjiuwen.studio.agent.runtime.service.md.adapter.req;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Component;
 
-/**
- * 默认 OpenAI 协议适配器, 同时作为未识别协议的 fallback 实现.
- */
-@Slf4j
 @Component("openApiRequestAdapterRun")
 public class OpenApiRequestAdapter extends AbstractRequestAdapter {
     @Override
