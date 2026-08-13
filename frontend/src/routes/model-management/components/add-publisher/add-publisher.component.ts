29cb6f1f9f70e933627ebe54f173732e2f0a6952
chenmingjie100 <c18761153049@163.com>
2026-08-13 21:32:26 +0800

    fix: 自定义ApiKey参数名称和参数值添加必填校验提示，Api-key提示中文化

--- frontend/src/routes/model-management/components/add-publisher/add-publisher.component.ts
+++ frontend/src/routes/model-management/components/add-publisher/add-publisher.component.ts
@@ -136,6 +136,7 @@ export class AddPublisherComponent implements OnInit {
   collapsed = false;
 
   apiKeyAuthArgs = [{ target_name: '', auth_key: '' }];
+  customApikeySubmitted = false;
 
   verifyMethods = [
     { label: this.i18n.transform('verify_methods.iam_username_and_password'), id: 'iam' },
@@ -410,6 +411,7 @@ export class AddPublisherComponent implements OnInit {
   }
 
   createPublisher() {
+    this.customApikeySubmitted = true;
     const isMainValid = this.checkGroup(this.myForm);
     const isCustomValid = this.myForm.value.auth_type === 'CUSTOM_APIKEY' ? this.checkGroup(this.authkeyForm?.form) : true;
 
