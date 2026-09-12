e316df4fba5ee4c90e1cbc6feb62b237867dbee5
jiyongbo <jiyongbo020@163.com>
2026-09-12 20:27:29 +0800

    fix:模型思考标签传参

--- agent-runtime/agent_runtime/ir_config/model_config.py
+++ agent-runtime/agent_runtime/ir_config/model_config.py
@@ -145,6 +145,7 @@ class IRModelConfigProvider(ModelConfigProvider):
             extra_body = {
                 "thinking": thinking,
                 "enable_thinking": thinking["type"] == "enabled",
+                "chat_template_kwargs": {"enable_thinking": thinking["type"] == "enabled"}
             }
 
         model_request_config = ModelRequestConfig(
